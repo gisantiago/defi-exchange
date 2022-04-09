b1b7c901b8cef7b621f364659ef8fbdf86db2dc0
gisantiago <gisantiago@live.com>
2022-04-08 20:26:25 -0400

    started adding utils functions to the frontend index.js

---
+++ my-app/pages/index.js
@@ -0,0 +1,181 @@
+import { BigNumber, providers, utils } from 'ethers';
+import Head from 'next/head';
+import React, { useEffect, useRef, useState } from 'react';
+import Web3Modal from 'web3modal';
+import styles from '../styles/Home.module.css';
+import { addLiquidity, calculateCD } from '../utils/addLiquidity';
+import {
+  getCDTokensBalance,
+  getEtherBalance,
+  getLPTokensBalance,
+  getReserveOfCDTokens,
+} from '../utils/getAmounts';
+import {
+  getTokensAfterRemove,
+  removeLiquidity,
+} from '../utils/removeLiquidity';
+import { swapTokens, getAmountOfTokensReceivedFromSwap } from '../utils/swap';
+
+export default function Home() {
+  /** General state variables */
+  // loading is set to true when the transaction is mining and set to false when the transaction has mined
+  const [loading, setLoading] = useState(false);
+  // We have two tabs in this dapp, Liquidity Tab and Swap Tab. This variable keeps track of which Tab the user is on
+  // If it is set to true this means that the user is on `liquidity` tab else he is on `swap` tab
+  const [liquidityTab, setLiquidityTab] = useState(true);
+  // This variable is the `0` number in form of a BigNumber
+  const zero = BigNumber.from(0);
+  /** Variables to keep track of amount */
+  // `ethBalance` keeps track of the amount of Eth held by the user's account
+  const [ethBalance, setEtherBalance] = useState(zero);
+  // `reservedCD` keeps track of the Crypto Dev tokens Reserve balance in the Exchange contract
+  const [reservedCD, setReservedCD] = useState(zero);
+  // Keeps track of the ether balance in the contract
+  const [etherBalanceContract, setEtherBalanceContract] = useState(zero);
+  // cdBalance is the amount of `CD` tokens help by the users account
+  const [cdBalance, setCDBalance] = useState(zero);
+  // `lpBalance` is the amount of LP tokens held by the users account
+  const [lpBalance, setLPBalance] = useState(zero);
+  /** Variables to keep track of liquidity to be added or removed */
+  // addEther is the amount of Ether that the user wants to add to the liquidity
+  const [addEther, setAddEther] = useState(zero);
+  // addCDTokens keeps track of the amount of CD tokens that the user wants to add to the liquidity
+  // in case when there is no initial liquidity and after liquidity gets added it keeps track of the
+  // CD tokens that the user can add given a certain amount of ether
+  const [addCDTokens, setAddCDTokens] = useState(zero);
+  // removeEther is the amount of `Ether` that would be sent back to the user based on a certain number of `LP` tokens
+  const [removeEther, setRemoveEther] = useState(zero);
+  // removeCD is the amount of `Crypto Dev` tokens that would be sent back to the user base on a certain number of `LP` tokens
+  // that he wants to withdraw
+  const [removeCD, setRemoveCD] = useState(zero);
+  // amount of LP tokens that the user wants to remove from liquidity
+  const [removeLPTokens, setRemoveLPTokens] = useState('0');
+  /** Variables to keep track of swap functionality */
+  // Amount that the user wants to swap
+  const [swapAmount, setSwapAmount] = useState('');
+  // This keeps track of the number of tokens that the user would recieve after a swap completes
+  const [tokenToBeRecievedAfterSwap, setTokenToBeRecievedAfterSwap] =
+    useState(zero);
+  // Keeps track of whether  `Eth` or `Crypto Dev` token is selected. If `Eth` is selected it means that the user
+  // wants to swap some `Eth` for some `Crypto Dev` tokens and vice versa if `Eth` is not selected
+  const [ethSelected, setEthSelected] = useState(true);
+  /** Wallet connection */
+  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
+  const web3ModalRef = useRef();
+  // walletConnected keep track of whether the user's wallet is connected or not
+  const [walletConnected, setWalletConnected] = useState(false);
+
+  /**
+   * getAmounts call various functions to retrive amounts for ethbalance,
+   * LP tokens etc
+   */
+  const getAmounts = async () => {
+    try {
+      const provider = await getProvideOrSigner(false);
+      const signer = await getProvideOrSigner(true);
+      const address = await signer.getAddress();
+      // get the amount of eth in the user's account
+      const _ethBalance = await getEtherBalance(provider, address);
+      // get the amount of `Crypto Dev` tokens held by the user
+      const _cdBalance = await getCDTokensBalance(provider, address);
+      // get the amount of `Crypto Dev` LP tokens held by the user
+      const _lpBalance = await getLPTokensBalance(provider, address);
+      // gets the amount of `CD` tokens that are present in the reserve of the `Exchange contract`
+      const _reservedCD = await getReserveOfCDTokens(provider);
+      // Get the ether reserves in the contract
+      const _ethBalanceContract = await getEtherBalance(provider, null, true);
+      setEtherBalance(_ethBalance);
+      setCDBalance(_cdBalance);
+      setLPBalance(_lpBalance);
+      setReservedCD(_reservedCD);
+      setReservedCD(_reservedCD);
+      setEtherBalanceContract(_ethBalanceContract);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  /**** SWAP FUNCTIONS ****/
+
+  /*
+  swapTokens: Swaps  `swapAmountWei` of Eth/Crypto Dev tokens with `tokenToBeRecievedAfterSwap` amount of Eth/Crypto Dev tokens.
+  */
+
+  const _swapTokens = async () => {
+    try {
+      // Convert the amount entered by the user to a BigNumber using the `parseEther` library from `ethers.js`
+      const swapAmountWei = utils.parseEther(swapAmount);
+      // Check if the user entered zero
+      // We are here using the `eq` method from BigNumber class in `ethers.js`
+      if (!swapAmountWei.eq(zero)) {
+        const signer = await getProvideOrSigner(true);
+        setLoading(true);
+        // Call the swapTokens function from the `utils` folder
+        await swapTokens(
+          signer,
+          swapAmountWei,
+          tokenToBeRecievedAfterSwap,
+          setEthSelected
+        );
+        setLoading(false);
+        // Get all the updated amounts after the swap
+        await getAmounts();
+        setSwapAmount('');
+      }
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+      setSwapAmount('');
+    }
+  };
+
+  /*
+    _getAmountOfTokensReceivedFromSwap:  Returns the number of Eth/Crypto Dev tokens that can be recieved 
+    when the user swaps `_swapAmountWEI` amount of Eth/Crypto Dev tokens.
+    */
+
+  const _getAmountOfTokensReceivedFromSwap = async (_swapAmount) => {
+    try {
+      // Convert the amount entered by the user to a BigNumber using the `parseEther` library from `ethers.js`
+      const _swapAmountWEI = utils.parseEther(_swapAmount.toString());
+      // Check if the user entered zero
+      // We are here using the `eq` method from BigNumber class in `ethers.js`
+      if (!_swapAmountWEI.eq(zero)) {
+        const provider = await getProviderOrSigner();
+        // Get the amount of ether in the contract
+        const _ethBalance = await getEtherBalance(provider, null, true);
+        // Call the `getAmountOfTokensReceivedFromSwap` from the utils folder
+        const amountOfTokens = await getAmountOfTokensReceivedFromSwap(
+          _swapAmountWEI,
+          provider,
+          ethSelected,
+          _ethBalance,
+          reservedCD
+        );
+        setTokenToBeRecievedAfterSwap(amountOfTokens);
+      } else {
+        setTokenToBeRecievedAfterSwap(zero);
+      }
+    } catch (error) {
+        console.error(error)
+    }
+  };
+
+  /*** END ***/
+
+  /**** ADD LIQUIDITY FUNCTIONS ****/
+
+  /**
+   * _addLiquidity helps add liquidity to the exchange,
+   * If the user is adding initial liquidity, user decides the ether and CD tokens he wants to add
+   * to the exchange. If we he adding the liquidity after the initial liquidity has already been added
+   * then we calculate the crypto dev tokens he can add, given the eth he wants to add by keeping the ratios
+   * constant
+   */
+
+  return (
+    <div>
+      <h1>Page Under Constructions...</h1>
+    </div>
+  );
+}
